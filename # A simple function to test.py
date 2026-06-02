9776e80aa0549a54b49b8168b54d15b2112d0208
RileyMorris05 <Riley.Morris@student.nmt.edu>
2026-06-02 10:40:24 -0600

    testing

---
+++ # A simple function to test.py
@@ -0,0 +1,10 @@
+# A simple function to test
+def add(a, b):
+    return a + b
+
+# The test case function
+def test_add_success():
+    assert add(2, 3) == 5
+
+def test_add_failure():
+    assert add(2, 2) != 5
